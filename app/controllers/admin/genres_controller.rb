b0ac66b1df6e3faf75e41ede46f20c190505d7e8
tompi04 <126743702+tompi04@users.noreply.github.com>
2023-06-18 17:18:04 +0000

    [Update] ジャンル機能の作成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,39 @@
 class Admin::GenresController < ApplicationController
+
+  def index
+    @genres = Genre.all
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      flash[:notice] = "ジャンル登録が完了しました"
+      redirect_to admin_genres_path
+    else
+      redirect_to actionｔ: 'index'
+    end
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      flash[:notice] = "変更が完了しました"
+      redirect_to admin_genres_path
+    else
+      render :edit
+    end
+  end
+
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+
 end
